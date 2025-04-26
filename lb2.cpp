d1842d22c3d20e53edf711238e783b0452cb47ac
NataNataM <w3tpcl@mail.ru>
2025-04-26 19:21:31 +0300

    Update lb2.cpp

--- lb2.cpp
+++ lb2.cpp
@@ -3,8 +3,8 @@
 #include <math.h>
 #include <mkl.h>
 #include <windows.h>
-#include <limits.h>
-#include <omp.h>
+#include <vector>
+#include <thread>
 using namespace std;
 
 //создание двумерного динамического массива
@@ -17,7 +17,7 @@ complex<float>** create_matrix(int n) {
     }
     return a;
 }
- 
+
 //переумножение матриц 1 способом
 void mult1_matrix(complex<float>** A, complex<float>** B, complex<float>** C, int n) {
     for (int i = 0; i < n; ++i) {
@@ -42,10 +42,9 @@ void trans_matrix(complex<float>** a, int n) {
     }
 }
 
-//переумножение матриц 3 способом
-void mult3_matrix(complex<float>** A, complex<float>** B, complex<float>** C, int n) {
-#pragma omp parallel for
-    for (int i = 0; i < n; ++i) {
+// Функция для работы одного потока в mult3_matrix_threads
+void mult3_part(complex<float>** A, complex<float>** B, complex<float>** C, int n, int start_row, int end_row) {
+    for (int i = start_row; i < end_row; ++i) {
         for (int j = 0; j < n; ++j) {
             complex<float> sum = 0;
             for (int k = 0; k < n; ++k) {
@@ -53,6 +52,26 @@ void mult3_matrix(complex<float>** A, complex<float>** B, complex<float>** C, in
             }
             C[i][j] = sum;
         }
+    }
+}
+
+//переумножение матриц 3 способом с использованием thread
+void mult3_matrix_threads(complex<float>** A, complex<float>** B, complex<float>** C, int n, int num_threads = 4) {
+    vector<thread> threads;
+    int rows = n / num_threads;
+    for (int t = 0; t < num_threads; ++t) {
+        int start_row = t * rows;
+        int end_row;
+        if (t == num_threads - 1) {
+            end_row = n;
+        }
+        else {
+            end_row = (t + 1) * rows;
+        }
+        threads.emplace_back(mult3_part, A, B, C, n, start_row, end_row);
+    }
+    for (auto& th : threads) {
+        th.join();
     }
 }
 
@@ -70,7 +89,7 @@ bool sravn_matrix(complex<float>** a, complex<float>** b, int n) {
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "ru");
     const int N = 2048;
     complex<float>** a = create_matrix(N);
     complex<float>** b = create_matrix(N);
@@ -98,8 +117,8 @@ int main() {
     double t_count = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
     double p = 2.0 * pow(N, 3) / t_count * pow(10, -6);
     cout << "1 способ:" << endl;
-    cout << "Время, затраченное на умножение матриц: " << t_count << " секунд.\n";
-    cout << "Производительность в MFlops: " << p << endl << endl;
+    cout << "Время, затраченное на умножение матриц: " << t_count << " секунд\n";
+    cout << "Производительность: " << p << " MFlops" << endl << endl;
 
     //2 способ
     float alpha[] = { 1.0, 0.0 };
@@ -111,26 +130,26 @@ int main() {
     t_count = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
     p = 2.0 * pow(N, 3) / t_count * pow(10, -6);
     cout << "2 способ:" << endl;
-    cout << "Время, затраченное на умножение матриц: " << t_count << " секунд.\n";
-    cout << "Производительность в MFlops: " << p << endl << endl;
+    cout << "Время, затраченное на умножение матриц: " << t_count << " секунд\n";
+    cout << "Производительность: " << p << " MFlops" << endl << endl;
 
     //3 способ
     trans_matrix(b, N);
     QueryPerformanceFrequency(&freq);
     QueryPerformanceCounter(&start);
-    mult3_matrix(a, b, c3, N);
+    mult3_matrix_threads(a, b, c3, N, thread::hardware_concurrency());
     QueryPerformanceCounter(&end);
     t_count = (double)(end.QuadPart - start.QuadPart) / freq.QuadPart;
     p = 2.0 * pow(N, 3) / t_count * pow(10, -6);
     cout << "3 способ:" << endl;
-    cout << "Время, затраченное на умножение матриц: " << t_count << " секунд.\n";
-    cout << "Производительность в MFlops: " << p << endl << endl;
+    cout << "Время, затраченное на умножение матриц: " << t_count << " секунд\n";
+    cout << "Производительность: " << p << " MFlops" << endl << endl;
 
     //сравнение матриц 
     if (sravn_matrix(c3, c2, N) && sravn_matrix(c1, c2, N) && sravn_matrix(c3, c1, N))
-        cout << "Матрицы c1, с2 и с3 равны.\n";
+        cout << "Матрицы c1, с2 и с3 равны\n";
     else
-        cout << "Матрицы c1, с2 и с3 не равны!\n";
+        cout << "Матрицы c1, с2 и с3 не равны\n";
 
     cout << "Выполнила: Мангер Наталья Александровна, группа 090304-РПИа-о24\n";
     return 0;
